7944ca204c71450e980969333cc2f46168db293b
Lovsog <840018730@qq.com>
2023-03-09 00:02:53 +0800

    2023.03.08

---
+++ chatbot-api-application/src/main/java/cn/lovsog/chatbot/api/application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Lovsog
+ * 2023/3/9
+ **/
+package cn.lovsog.chatbot.api.application;
---
+++ chatbot-api-interfaces/src/main/java/cn/lovsog/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package cn.lovsog.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lovsog
+ * @description 启动入口
+ * 2023/3/8
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
