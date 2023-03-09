241cb28e0174c9bc368696d796a871e348c063ea
Lovsog <840018730@qq.com>
2023-03-09 10:15:35 +0800

    Lovsog feat:知识星球API调用

--- chatbot-api-interfaces/src/test/java/cn/lovsog/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/lovsog/chatbot/api/test/ApiTest.java
@@ -1,9 +1,69 @@
 package cn.lovsog.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * @author Lovsog
  * @description 单元测试
  * 2023/3/9
  **/
 public class ApiTest {
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/ /topics?scope=all&count=20");
+
+        get.addHeader("cookie","");
+        get.addHeader("Content-Type","application/json, text/plain, */*");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/ /answer");
+
+        post.addHeader("cookie","");
+        post.addHeader("Content-Type","application/json, text/plain, */*");
+
+        // silenced为true，仅提问者可见；为false，所有人可见
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"我也不晓得\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": true\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
